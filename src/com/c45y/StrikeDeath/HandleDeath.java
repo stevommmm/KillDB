be6bc9edddeca64837380ae3523318637387eb09
smcgregor <smcgregor@smcgregor-mint.(none)>
2012-02-21 08:13:36 +1000

    updated to new event system

--- src/com/c45y/StrikeDeath/HandleDeath.java
+++ src/com/c45y/StrikeDeath/HandleDeath.java
@@ -2,28 +2,28 @@ package com.c45y.StrikeDeath;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.entity.EntityListener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
-public class HandleDeath extends EntityListener
+public class HandleDeath implements Listener
 {
-  public StrikeDeath plugin;
+	public StrikeDeath plugin;
 
-  public HandleDeath(StrikeDeath instance)
-  {
-    this.plugin = instance;
-  }
+	public HandleDeath(StrikeDeath instance)
+	{
+		this.plugin = instance;
+	}
 
-  public void onEntityDeath(EntityDeathEvent event) {
-    if ((event instanceof PlayerDeathEvent)) {
-      Player player = (Player)event.getEntity();
-      if (((player.getKiller() instanceof Player)) && (
-        (player.getInventory().getChestplate().getType().getMaxDurability() > 150) || 
-        (player.getInventory().getLeggings().getType().getMaxDurability() > 115))) {
-        Location loc = new Location(player.getLocation().getWorld(), player.getLocation().getX(), player.getLocation().getY() + 5.0D, player.getLocation().getZ(), 360.0F, 0.0F);
-        event.getEntity().getWorld().strikeLightningEffect(loc);
-      }
-    }
-  }
+	public void onEntityDeath(EntityDeathEvent event) {
+		if ((event instanceof PlayerDeathEvent)) {
+			Player player = (Player)event.getEntity();
+			if (((player.getKiller() instanceof Player)) && (
+					(player.getInventory().getChestplate().getType().getMaxDurability() > 150) || 
+					(player.getInventory().getLeggings().getType().getMaxDurability() > 115))) {
+				Location loc = new Location(player.getLocation().getWorld(), player.getLocation().getX(), player.getLocation().getY() + 5.0D, player.getLocation().getZ(), 360.0F, 0.0F);
+				event.getEntity().getWorld().strikeLightningEffect(loc);
+			}
+		}
+	}
 }
--- src/com/c45y/StrikeDeath/StrikeDeath.java
+++ src/com/c45y/StrikeDeath/StrikeDeath.java
@@ -1,22 +1,22 @@
 package com.c45y.StrikeDeath;
 
 import java.util.logging.Logger;
-import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StrikeDeath extends JavaPlugin
 {
-  private final HandleDeath HandleDeath = new HandleDeath(this);
-  Logger log = Logger.getLogger("Minecraft");
+	private final HandleDeath HandleDeath = new HandleDeath(this);
+	Logger log = Logger.getLogger("Minecraft");
 
-  public void onEnable() {
-    PluginManager pm = getServer().getPluginManager();
-    pm.registerEvent(Event.Type.ENTITY_DEATH, this.HandleDeath, Event.Priority.Normal, this);
-    this.log.info("StrikeDeath enabled.");
-  }
+	public void onEnable() {
+		PluginManager pm = getServer().getPluginManager();
+		//pm.registerEvent(Event.Type.ENTITY_DEATH, this.HandleDeath, Event.Priority.Normal, this);
+		pm.registerEvents(HandleDeath, this);
+		this.log.info("StrikeDeath enabled.");
+	}
 
-  public void onDisable() {
-    this.log.info("StrikeDeath disabled.");
-  }
+	public void onDisable() {
+		this.log.info("StrikeDeath disabled.");
+	}
 }
